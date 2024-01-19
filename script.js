c994e717e7f479eebdc522e662c96e4b36c63dc4
Brandon Knight <knightbrandon1984@gmail.com>
2024-01-19 00:33:48 -0600

    done

--- script.js
+++ script.js
@@ -552,16 +552,19 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdrawl(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
     if (this.#approveLoan(val)) {
       this.deposit(val);
       console.log('Loan approved!');
+      return this;
     }
   }
 
@@ -588,3 +591,93 @@ console.log(acc1.getMovements());
 
 // console.log(acc1.#movements); //
 Account.helper();
+
+// ========================================================
+// ========================================================
+// ========================================================
+//   -----------       CHAINING METHODS       -----------
+// ========================================================
+// ========================================================
+// ========================================================
+
+acc1.deposit(300).deposit(500).withdrawl(35).requestLoan(25000).withdrawl(4000);
+
+console.log(acc1.getMovements());
+
+// ========================================================
+// ========================================================
+// ========================================================
+//   -----------       CODING CHALLENGE #4       -----------
+// ========================================================
+// ========================================================
+// ========================================================
+
+// Your tasks:
+// 1. Re-create Challenge #3,but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+// 2. Make the 'charge' property private
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl' class. Then experiment with chaining!
+// Test data:
+// - Data car 1: 'Rivian' going at 120 km/h, with a charge of 23% GOOD LUCK 😀
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h.`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h.`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    const carSpeed = (this.speed += 20);
+    const carCharge = this.#charge--;
+    console.log(
+      `${this.make} is going ${carSpeed} km/h, with a charge of ${carCharge}%.`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+// - Data car 1: 'Rivian' going at 120 km/h, with a charge of 23% GOOD LUCK 😀
+// console.log(rivian);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
